720a4f49e7fea3d389c9b7d8df3dc3cdf8d4f7fa
1637467054 <1637467054@qq.com>
2020-08-04 20:35:57 +0800

    1、简化登录页面代码 2、增加eslintrc.js，去掉部分检查项 解决修改代码自动编译时报错：$ in not defined

---
+++ admin/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    'extends': [
+        'plugin:vue/essential',
+        'eslint:recommended'
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-undef': 'off',//如果开启这个检查项的话跟新就会报错
+        'vue/no-unused-vars': 'off',
+        'vue/require-v-for-key': 'off',
+        'no-unused-vars': 'off',
+        'vue/no-unused-components': 'off'
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    }
+};
